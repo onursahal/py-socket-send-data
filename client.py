b9800f3bc6b8d7e8c5a9ed75c32417b46a1a3738
Onur Şahal <sahalonur@gmail.com>
2019-10-16 13:11:13 +0300

    object and message change

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1234))
+newsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+newsocket.connect((socket.gethostname(), 1234))
 
-msg = s.recv(1024)
+msg = newsocket.recv(1024)
 print(msg.decode("utf-8"))
--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1234))
-s.listen(5)
+newsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+newsocket.bind((socket.gethostname(), 1234))
+newsocket.listen(5)
 
 while True:
-    clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established!")
-    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
+    clientsocket, address = newsocket.accept()
+    print(f"{address} adresindeki bağlantı kuruldu!")
+    clientsocket.send(bytes("Server'a hoşgeldiniz! (Bu yollanan bir veridir.)", "utf-8"))
